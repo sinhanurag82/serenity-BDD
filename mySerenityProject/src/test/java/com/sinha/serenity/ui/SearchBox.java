a8c01199a1d262504741ebb5798a16787c380ac5
Anurag Sinha <anuragsinha@anurags-mbp.home>
2019-07-05 11:03:20 +0100

    add serenity project structure

---
+++ mySerenityProject/src/test/java/com/sinha/serenity/ui/SearchBox.java
@@ -0,0 +1,8 @@
+package com.sinha.serenity.ui;
+
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
+
+public class SearchBox {
+    public static Target SEARCH_FIELD = Target.the("search field").located(By.name("q"));
+}
